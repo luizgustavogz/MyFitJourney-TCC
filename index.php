355b35c0e350bf071a4539675d776156148a7990
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-09-17 12:14:20 -0300

    remove files

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyFitJourney | Evolua para a sua melhor versão</title>
     <link rel="stylesheet" href="./assets/css/index.scss">
-    <link rel="stylesheet" href="./assets/css/footer.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.26/dist/css/uikit.min.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/footer.css">    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.26/dist/css/uikit.min.css" />    
+        
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script src="./assets/js/map.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.26/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.26/dist/js/uikit-icons.min.js"></script>
 </head>
 <body>
     <section id="menu-nav-home-banner">
@@ -207,5 +206,3 @@
     </footer>
 </body>
 </html>
-<script src="https://cdn.jsdelivr.net/npm/uikit@3.16.26/dist/js/uikit.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/uikit@3.16.26/dist/js/uikit-icons.min.js"></script>
