45170599aae4d0de1d889595caa340d357612e9a
Abner Freitas <freitasabner12@gmail.com>
2023-11-22 17:22:24 -0300

    Card Payment creation

---
+++ assets/js/payment.js
@@ -0,0 +1,35 @@
+document.querySelector('.card-number-input').oninput = () => {
+    document.querySelector('.card-number-box').innerText = document.querySelector('.card-number-input').value;
+    if (!document.querySelector('.card-number-input').value){
+        document.querySelector('.card-number-box').innerText = '################'
+    }
+}
+
+document.querySelector('.card-holder-input').oninput = () => {
+    document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value;
+    if (!document.querySelector('.card-holder-input').value){
+        document.querySelector('.card-holder-name').innerText = 'Nome completo'
+    }
+}
+
+document.querySelector('.month-input').oninput = () => {
+    document.querySelector('.exp-month').innerText = document.querySelector('.month-input').value;
+}
+
+document.querySelector('.year-input').oninput = () => {
+    document.querySelector('.exp-year').innerText = document.querySelector('.year-input').value;
+}
+
+document.querySelector('.cvv-input').onmouseenter = () => {
+    document.querySelector('.card').style.transform = 'perspective(1000px) rotateY(-180deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(0deg)';
+}
+
+document.querySelector('.cvv-input').onmouseleave = () => {
+    document.querySelector('.card').style.transform = 'perspective(1000px) rotateY(0deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(180deg)';
+}
+
+document.querySelector('.cvv-input').oninput = () => {
+    document.querySelector('.cvv-box').innerText = document.querySelector('.cvv-input').value;
+}
