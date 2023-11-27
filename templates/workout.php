f6013eadb8ec24948f0694cce123f935d5b0f571
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2023-11-27 12:33:04 -0300

    update workout.css

--- templates/workout.php
+++ templates/workout.php
@@ -163,7 +163,7 @@
 
             <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid uk-scrollspy="cls: uk-animation-slide-bottom; target: .uk-card; delay: 500; repeat: true">
                 <div>
-                    <div class="uk-card uk-card-body card-benefits cards-border-black" id="cardiovascular">
+                    <div class="uk-card uk-card-body card-benefits" id="cardiovascular">
                         <h3 class="uk-card-title">
                             <img src="../assets/img/img-wk-card-cardio.png" alt="" width="55" height="55">
                             Cardiovascular
@@ -191,7 +191,7 @@
                     </div>
                 </div>
                 <div>
-                    <div class="uk-card uk-card-body card-benefits-secondary cards-border-white" uk-scrollspy-class="uk-animation-slide-top" id="resistencia">
+                    <div class="uk-card uk-card-body card-benefits-secondary" uk-scrollspy-class="uk-animation-slide-top" id="resistencia">
                         <h3 class="uk-card-title">
                             <div class="invert-img">
                                 <canvas id="canvas-resistencia" width="55" height="55"></canvas>
@@ -221,7 +221,7 @@
                     </div>
                 </div>
                 <div>
-                    <div class="uk-card uk-card-body card-benefits cards-border-black" id="definicao">
+                    <div class="uk-card uk-card-body card-benefits" id="definicao">
                         <h3 class="uk-card-title">
                             <img src="../assets/img/img-wk-card-definicao.png" alt="" width="55" height="55">
                             Definição
@@ -254,7 +254,7 @@
 
             <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid uk-scrollspy="cls: uk-animation-slide-top; target: .uk-card; delay: 500; repeat: true">
                 <div>
-                    <div class="uk-card uk-card-body card-benefits-secondary cards-border-white" id="forca">
+                    <div class="uk-card uk-card-body card-benefits-secondary" id="forca">
                         <h3 class="uk-card-title">
                             <div class="invert-img">
                                 <canvas id="canvas-forca" width="55" height="55"></canvas>
@@ -283,7 +283,7 @@
                     </div>
                 </div>
                 <div>
-                    <div class="uk-card uk-card-body card-benefits cards-border-black" uk-scrollspy-class="uk-animation-slide-bottom" id="funcional" >
+                    <div class="uk-card uk-card-body card-benefits" uk-scrollspy-class="uk-animation-slide-bottom" id="funcional" >
                         <h3 class="uk-card-title">
                             <img src="../assets/img/img-wk-card-funcional.png" alt="" width="55" height="55">
                             Funcional
@@ -310,7 +310,7 @@
                     </div>
                 </div>
                 <div>
-                    <div class="uk-card uk-card-body card-benefits-secondary cards-border-white" id="hipertrofia"> 
+                    <div class="uk-card uk-card-body card-benefits-secondary" id="hipertrofia"> 
                         <h3 class="uk-card-title">
                             <div class="invert-img">
                                 <canvas id="canvas-hipertrofia" width="55" height="55"></canvas>
