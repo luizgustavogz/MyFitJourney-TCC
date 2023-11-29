9c7508c1ddac4e72a035d00e3ce16cb5a665322a
Abner Freitas <freitasabner12@gmail.com>
2023-11-29 12:50:41 -0300

    Carrinho system update

--- assets/js/loja.js
+++ assets/js/loja.js
@@ -17,9 +17,10 @@ async function loadProducts(url) {
 
         const accImgElements = document.getElementsByClassName('uk-card uk-card-body card-benefits cards-border'); // Seleciona todas as divs com a classe 'accessories-img'
         const nameProducts = document.getElementsByClassName('uk-card-title');
-        const priceProducts = document.getElementsByClassName('products-price');
         const carrinho = document.getElementById('carrinho');
+        const priceProducts = document.getElementsByClassName('products-price');
         const total = document.getElementById('total_amount');
+        let carrinhoTexto = '';
 
         for (let i = 0; i < responseJson.products.length; i++) {
             const tagImg = document.createElement('img');
@@ -56,18 +57,41 @@ async function loadProducts(url) {
                 link.className = 'shoppingCart';
                 link.innerText = 'Adicionar no carrinho';
 
+
                 responseJson.products[i].quantity = 0;
                 link.addEventListener("click", function () {
+                    console.log(responseJson.products[i].name, responseJson.products[i].price)
+
+                    // Incrementa a quantidade do produto
                     responseJson.products[i].quantity += 1;
-                    console.log(responseJson.products[i].name && responseJson.products[i].price)
 
-                    total.innerText = ` ${((responseJson.products[i].price)*(responseJson.products[i].quantity)).toFixed(2)}`
-                })
+                    // Atualiza o texto do carrinho
+                    carrinhoTexto = responseJson.products.reduce((acumulador, produto) => {
+                        if (produto.quantity > 0) {
+                            acumulador += `${produto.name} (${produto.quantity}) <br>`;
+                        }
+                        return acumulador;
+                    }, '');
+
+                    // Atualiza a exibição do carrinho na tela
+                    carrinho.style.fontSize = '13px';
+                    carrinho.innerHTML = carrinhoTexto;
+
+                    // Atualiza o total na tela
+                    total.innerText = `R$ ${calcularTotal().toFixed(2)}`;
+                });
             }
-
         }
 
-
+        function calcularTotal() {
+            return responseJson.products.reduce((acc, produto) => {
+                // Garante que os valores sejam números válidos
+                const price = parseFloat(produto.price) || 0;
+                const quantity = parseInt(produto.quantity) || 0;
+        
+                return acc + (price * quantity);
+            }, 0);
+        }
 
     } catch (error) {
         alert(error)
